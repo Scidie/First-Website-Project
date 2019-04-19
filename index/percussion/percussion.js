e12dd479de8a8276c21791d07a6c23e23d9836fa
Scidie <patrykblaszczak1@gmail.com>
2019-04-19 12:44:55 +0200

    uncomment percussion js

--- index/percussion/percussion.js
+++ index/percussion/percussion.js
@@ -1,23 +1,23 @@
-// window.addEventListener('keydown', function(e) {
-//     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
-//     const key = document.querySelector(`span[data-key="${e.keyCode}"]`)
-//     key.classList.add('playing');
-//     audio.currentTime = 0;
-//     audio.play();
-// });
+window.addEventListener('keydown', function(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    const key = document.querySelector(`span[data-key="${e.keyCode}"]`)
+    key.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
+});
 
-// function removeTransition (e) {
-//     e.target.classList.remove('playing');
-// }
+function removeTransition (e) {
+    e.target.classList.remove('playing');
+}
 
-// const keys = Array.from(document.querySelectorAll('.key'));
-// keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 
-// //Event listener for click event__________________________________________________________________________
-// window.addEventListener('click', function (e) {
-//     const key = document.querySelector(`span[id="${e.target.id}"]`)
-//     const audio = document.querySelector(`audio[data-key="${key.getAttribute('data-key')}"]`);
-//     key.classList.add('playing');
-//     audio.currentTime = 0;
-//     audio.play();
-// });
+//Event listener for click event__________________________________________________________________________
+window.addEventListener('click', function (e) {
+    const key = document.querySelector(`span[id="${e.target.id}"]`)
+    const audio = document.querySelector(`audio[data-key="${key.getAttribute('data-key')}"]`);
+    key.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
+});
