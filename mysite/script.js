d309aa49a22503d387943ed8899ed867cfad158a
MX <4masmix1@gmail.com>
2019-04-22 13:09:43 +0200

    Fix date - displaying mins

--- mysite/script.js
+++ mysite/script.js
@@ -4,9 +4,13 @@ var updateClock = function() {
     let hours = date.getHours();
     let minutes = date.getMinutes();
     let seconds = date.getSeconds();
+
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
     let dateFormatted = hours + ":" + minutes + ":" + seconds;
 
     document.getElementById("header-date-holder").innerText = dateFormatted;
 };
-
+updateClock();
 setInterval(updateClock, 1000);
