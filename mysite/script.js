7a764592de4d15d89f09bf70cc6339a566d2334e
MX <4masmix1@gmail.com>
2019-04-22 13:14:43 +0200

    Fix date - zeroes not showing when value is less than 10

--- mysite/script.js
+++ mysite/script.js
@@ -5,9 +5,18 @@ var updateClock = function() {
     let minutes = date.getMinutes();
     let seconds = date.getSeconds();
 
+    if (hours < 10) {
+        hours = "0" + hours;
+    }
+
     if (minutes < 10) {
         minutes = "0" + minutes;
     }
+
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+
     let dateFormatted = hours + ":" + minutes + ":" + seconds;
 
     document.getElementById("header-date-holder").innerText = dateFormatted;
